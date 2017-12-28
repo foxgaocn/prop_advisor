d9cdb58893f242dbb1a4a62e646984861189159b
harry gao <foxgaocn@gmail.com>
2017-12-28 22:18:00 +1100

    fixed a few bugs

--- apps/advisor/lib/advisor/fsm.ex
+++ apps/advisor/lib/advisor/fsm.ex
@@ -7,17 +7,17 @@ defmodule Advisor.Fsm do
 
   #:restart -> :purpose
   def next(_, "restart") do
-    {Questions.get(:restart), %{step: :purpose}}
+    {Questions.get(:restart), %{step: :purpose, previous_step: :greeting}}
   end
 
   #:start -> :purpose
   def next(%{step: :start}, _) do
-    {Questions.get(:start), %{step: :purpose}}
+    {Questions.get(:start), %{step: :purpose, previous_step: :greeting}}
   end
 
   #:greeting -> :purpose
   def next(%{step: :greeting}, _) do
-    {Questions.get(:greeting), %{step: :purpose}}
+    {Questions.get(:greeting), %{step: :purpose, previous_step: :greeting}}
   end
 
   # :purpose -> :province
@@ -27,7 +27,7 @@ defmodule Advisor.Fsm do
     case message do
       "resi" -> { next_question, %{step: :province, purpose: "resi"} }
       "inv" -> { next_question, %{step: :province, purpose: "inv"} }
-      "back" -> { Questions.get(:greeting), state }
+      "back" -> go_back(state)
       _ -> { @retry, state }
     end
   end
@@ -37,8 +37,9 @@ defmodule Advisor.Fsm do
     next_question = Questions.get(:province)
     
     cond do
-      Enum.member?(["nsw", "vic", "qld", "nt", "tas", "sa", "wa"], message) -> { next_question, Map.merge(state, %{step: :budget, state: message})}
-      message == "back" -> {Questions.get(:purpose)}
+      Enum.member?(["nsw", "vic", "qld", "nt", "tas", "sa", "wa"], message) -> 
+        { next_question, Map.merge(state, %{step: :budget, previous_step: :province, state: message})}
+      message == "back" -> {Questions.get(:purpose), state}
       true -> { @retry, state }
     end
   end
@@ -47,11 +48,11 @@ defmodule Advisor.Fsm do
   def next(%{step: :budget} = state, message) do
     next_question = Questions.get(:budget)
     if (message == "back") do
-      { Questions.get(:province), state }
+      go_back(state)
     else
       case Integer.parse(message) do
         :error -> { @retry, state }
-        {budget, _} -> { next_question, Map.merge(state, %{budget: budget, step: :where})}
+        {budget, _} -> { next_question, Map.merge(state, %{budget: budget, step: :where, previous_step: :budget})}
       end
     end
   end
@@ -61,13 +62,13 @@ defmodule Advisor.Fsm do
     case message do
       "yes" -> 
         next_question = Questions.get(:suburb_yes)
-        next_state = Map.merge(state, %{where: "yes", step: :suburb, previous_step: :where})
+        next_state = Map.merge(state, %{where: "yes", step: :suburb, previous_step: :suburb_yes})
         {next_question, next_state}
       "no" ->
         next_question = Questions.get(:suburb_no)
-        next_state = Map.merge(state, %{where: "no", step: :location})
+        next_state = Map.merge(state, %{where: "no", step: :location, previous_step: :suburb_no})
         {next_question, next_state}
-      "back" -> { Questions.get(:budget), state }
+      "back" -> go_back(state)
       _ -> { @retry, state }
     end
   end
@@ -75,30 +76,31 @@ defmodule Advisor.Fsm do
   # :suburb -> :what
   def next(%{step: :suburb} = state, message) do
     if(message == "back") do
-      {get_previous_question(state), state}
+      go_back(state)
     else
       suburbs = Suburb.find_suburbs(message, Map.get(state, :state))
       case suburbs do
         [] -> { @retry, state }
         _ -> 
           next_question = Questions.get(:suburb)
-          next_state = Map.merge(state, %{suburbs: suburbs, previou_step: :suburb, step: :what})
+          next_state = Map.merge(state, %{previous_step: :suburb, step: :what, suburbs: suburbs})
           {next_question, next_state}
       end
     end
   end
 
+  # :what -> :bedroom | :commute_to
   def next(%{step: :what} = state, message) do
     case message do
       "yes" -> 
         next_question = Questions.get(:what_yes)
-        next_state = Map.merge(state, %{what: "yes", step: :finish, previous_step: :where})
+        next_state = Map.merge(state, %{what: "yes", step: :finish, previous_step: :what_yes})
         {next_question, next_state}
       "no" ->
         next_question = Questions.get(:what_no)
-        next_state = Map.merge(state, %{what: "no", step: :finish})
+        next_state = Map.merge(state, %{what: "no", step: :finish, previous_step: :what_no})
         {next_question, next_state}
-      "back" -> { Questions.get(:budget), state }
+      "back" -> go_back(state)
       _ -> { @retry, state }
     end
   end
@@ -110,8 +112,9 @@ defmodule Advisor.Fsm do
     {next_question, state}
   end
 
-  defp get_previous_question(state) do
-    state |> Map.get(:previous_step) |> Questions.get
+  defp go_back(state) do
+    #IO.inspect(state)
+    {state |> Map.get(:previous_step) |> Questions.get , state}
   end
   
 end
